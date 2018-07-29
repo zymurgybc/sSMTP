77f576ee35d243ae7d1db8b7eddc31359fe2236f
Ted H <zymurgy.bc@gmail.com>
2018-07-29 16:06:32 -0700

    add script for my preferred build steps

---
+++ my_build.sh
@@ -0,0 +1,20 @@
+./configure --enable-ssl --prefix=/usr --exec-prefix=/usr/sbin --sysconfdir=/etc
+
+if [ -f "/etc/ssmtp/revaliases" ]; then
+   if [ ! -f "/etc/ssmtp/revaliases.bak" ]; then
+      sudo cp /etc/ssmtp/revaliases /etc/ssmtp/revaliases.bak
+   fi
+fi
+
+make
+sudo make install
+
+sudo chmod 2755 /usr/sbin/ssmtp
+sudo chgrp mail /usr/sbin/ssmtp
+
+if [ -f "/etc/ssmtp/revaliases.bak" ]; then
+   sudo cp /etc/ssmtp/revaliases.bak /etc/ssmtp/revaliases
+fi
+
+# For each user:
+# sudo usermod -a -G mail ${userid}

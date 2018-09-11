48fe43c8fcfadf129530cf987ac51fc87af92fb8
Ted H <zymurgy.bc@gmail.com>
2018-09-10 18:26:24 -0700

    cleaning up some config setup lines since this box works and others do not.

--- my_build.sh
+++ my_build.sh
@@ -1,4 +1,4 @@
-./configure --enable-ssl --prefix=/usr --exec-prefix=/usr/sbin --sysconfdir=/etc
+./configure --enable-ssl --enable-inet6 --prefix=/usr --exec-prefix=/usr/sbin --sysconfdir=/etc
 
 if [ -f "/etc/ssmtp/revaliases" ]; then
    if [ ! -f "/etc/ssmtp/revaliases.bak" ]; then
@@ -6,15 +6,18 @@ if [ -f "/etc/ssmtp/revaliases" ]; then
    fi
 fi
 
-make
+make clean && make
 sudo make install
-
-sudo chgrp mail /usr/sbin/ssmtp
-sudo chmod 2755 /usr/sbin/ssmtp
 
 if [ -f "/etc/ssmtp/revaliases.bak" ]; then
    sudo cp /etc/ssmtp/revaliases.bak /etc/ssmtp/revaliases
 fi
+
+sudo chgrp mail /etc/ssmtp/*
+sudo chmod  640 /etc/ssmtp/*
+sudo chmod  644 /etc/ssmtp/revaliases
+sudo chgrp mail /usr/sbin/ssmtp
+sudo chmod 2555 /usr/sbin/ssmtp
 
 # For each user:
 # sudo usermod -a -G mail ${userid}
